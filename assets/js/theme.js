73c1eb677b26cdd3e355aad399ea6f47a1a59ecb
yao910509 <coutoucluiyao@gmail.com>
2025-03-20 15:04:07 -0500

    Added custom scrollbar in product grid

---
+++ assets/js/theme.js
@@ -0,0 +1,34 @@
+const scrollbarThumb = document.getElementById("customScrollbarThumb");
+const productGrid = document.getElementById("productGrid");
+const scrollbarTrack = document.getElementById("customScrollbar");
+
+scrollbarThumb.addEventListener("mousedown", function (event) {
+  var startX = event.clientX;
+  var startLeft = scrollbarThumb.offsetLeft;
+
+  document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
+
+  function onMouseMove(event) {
+    var deltaX = event.clientX - startX;
+    var newLeft = startLeft + deltaX;
+
+    if (newLeft < 0) {
+      newLeft = 0;
+    } else if (newLeft > scrollbarTrack.offsetWidth - scrollbarThumb.offsetWidth) {
+      newLeft = scrollbarTrack.offsetWidth - scrollbarThumb.offsetWidth;
+    }
+
+    scrollbarThumb.style.left = newLeft + "px";
+    scrollbarThumb.style.left = newLeft + "px";
+
+    var scrollRatio = newLeft / (scrollbarTrack.offsetWidth - scrollbarThumb.offsetWidth);
+    var maxScroll = productGrid.scrollWidth - productGrid.clientWidth;
+    productGrid.scrollLeft = scrollRatio * maxScroll;
+  }
+
+  function onMouseUp() {
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseup", onMouseUp);
+  }
+});
